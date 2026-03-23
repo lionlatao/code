672a48888672fbbee8d55d66bef2941e935a5630
lionlataoo <quynhthtt999@gmail.com>
2026-03-23 22:01:38 +0700

    fix2

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 
-  const startMessage = "Click here to see the motion baby";
+  const startMessage = "Click here to see the motion baby, quay ngang để hiển thị tốt nhất";
   let startTextContent = '';
   let startIndex = 0;
   let startCursorVisible = true;
